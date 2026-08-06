c3fc66ed8f8b1a7e9e4cef492e7be06a92b3cdf7
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2026-08-06 16:30:27 +0200

    feat(map): implement file loading

--- src/test/scala/it/unibo/pps/scalaman/map/io/MapLoaderSpec.scala
+++ src/test/scala/it/unibo/pps/scalaman/map/io/MapLoaderSpec.scala
@@ -1,5 +1,6 @@
 package it.unibo.pps.scalaman.map.io
 
+import java.nio.file.Files
 import java.nio.file.Paths
 
 import it.unibo.pps.scalaman.model.map.MapLoadError
@@ -23,5 +24,27 @@ class MapLoaderSpec extends AnyFunSuite, MapTestSupport:
       case MapLoadError.FileNotFound(_) => true
       case _ => false
     })
+  }
+
+  test("loads an empty file as empty raw text") {
+    val emptyFile = Files.createTempFile("map-loader-empty", ".txt")
+    try {
+      val loaded = MapLoader.load(emptyFile)
+
+      assert(loaded == Right(""))
+    } finally Files.deleteIfExists(emptyFile)
+  }
+
+  test("fails when the path is not a readable file") {
+    val unreadable = Files.createTempDirectory("map-loader-unreadable")
+    try {
+      val loaded = MapLoader.load(unreadable)
+
+      assert(loaded.isLeft)
+      assert(loaded.left.exists {
+        case MapLoadError.ReadFailed(_, _) => true
+        case _ => false
+      })
+    } finally Files.deleteIfExists(unreadable)
   }
 end MapLoaderSpec
