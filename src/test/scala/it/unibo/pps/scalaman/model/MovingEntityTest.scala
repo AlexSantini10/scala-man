1eafca9efd9a357b78374dde973c453d1baf9dd7
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2026-08-08 17:11:48 +0200

    feat(movement): improve tests for MovingEntity

--- src/test/scala/it/unibo/pps/scalaman/model/MovingEntityTest.scala
+++ src/test/scala/it/unibo/pps/scalaman/model/MovingEntityTest.scala
@@ -9,7 +9,12 @@ class MovingEntityTest extends AnyFunSuite:
   private val millis = 100.millis
   private val startingCell = Cell(0, 0)
 
-  private val basicEntity = MovingEntity(startingCell, Left, millis, None)
+  private val basicEntity = MovingEntity(
+    startingCell,
+    Left,
+    millis,
+    Some(Movement(startingCell, startingCell + Left, millis))
+  )
   test("a moving entity that is not moving stays in its starting cell") {
     assert(basicEntity.currentCell == startingCell)
   }
@@ -29,4 +34,46 @@ class MovingEntityTest extends AnyFunSuite:
         assert(entity.movement.contains(Movement(startingCell, target, millis)))
         assert(entity.facing === direction)
       }
+  }
+
+  test("face changes the direction the entity is facing") {
+    assert(basicEntity.face(Left).facing == Left)
+  }
+
+  test("face does not change the movement in progress") {
+    assert(
+      basicEntity.move(Down).face(Left).movement == basicEntity
+        .move(Down)
+        .movement
+    )
+    assert(
+      basicEntity.move(Down).face(Left).currentCell == basicEntity
+        .move(Down)
+        .currentCell
+    )
+  }
+
+  test("update does nothing when the entity is not moving") {
+    val unmovingEntity = MovingEntity(startingCell, Left, millis, None)
+    assert(unmovingEntity.update(200.millis).movement.isEmpty)
+  }
+
+  test(
+    "update reduces the remaining time by the given amount when the movement is incomplete"
+  ) {
+    assert(
+      basicEntity
+        .update(50.millis)
+        .movement
+        .get
+        .remaining == basicEntity.movement.get.remaining - 50.millis
+    )
+  }
+
+  test(
+    "update makes the entity stop moving if the remaining time reaches zero"
+  ) {
+    assert(
+      basicEntity.update(basicEntity.movement.get.remaining).movement.isEmpty
+    )
   }
